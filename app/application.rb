707d58597c98549286c91cde0aa6a220253fe025
Trish Ogen <patriciaogen@gmail.com>
2019-07-25 00:22:47 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,6 @@
+# 1. Your application should only accept the `/items/<ITEM NAME>` route. Everything else should `404`
+# 2. If a user requests `/items/<Item Name>` it should return the price of that item
+# 3. IF a user requests an item that you don't have, then return a `400` and an error message
+
+class Application
+end
