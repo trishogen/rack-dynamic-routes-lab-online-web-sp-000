786934b99ac540d5e703ae0a1f09d1951812ad1e
Trish Ogen <patriciaogen@gmail.com>
2019-07-25 00:40:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,3 @@
-# 2. If a user requests `/items/<Item Name>` it should return the price of that item
-# 3. IF a user requests an item that you don't have, then return a `400` and an error message
-
 class Application
 
   @@items = []
@@ -11,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       search_item = req.path.split("/items/").last
-      binding.pry
+
       if @@items.include?(search_item)
         item = @@items.select {|item| item.name == search_item}
         print item
@@ -20,6 +17,7 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       end
+      
     else
       resp.write "Route not found"
       resp.status = 404
