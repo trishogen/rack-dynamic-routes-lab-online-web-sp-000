5d1386f8007d3cd4d5e123dd4986fc2e93127b6b
Trish Ogen <patriciaogen@gmail.com>
2019-07-25 00:26:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,9 @@ class Application
   @@items = []
 
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
     if req.path.match(/items/)
       resp.write "good"
     else
