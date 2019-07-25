8cd2da7b0bd16d74eb025c389f00c03f40914591
Trish Ogen <patriciaogen@gmail.com>
2019-07-25 00:27:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,13 +9,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path.match(/items/)
       resp.write "good"
     else
       resp.write "Route not found"
       resp.status = 404
     end
+
+    resp.finish
   end
 
 end
